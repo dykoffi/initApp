044e82efc39e2202edbabba6120d484d31d9db49
dykoffi <koffiedy@gmail.com>
2020-03-07 22:01:07 +0000

    isme init

---
+++ bin/www.js
@@ -0,0 +1,98 @@
+var app = require('../app');
+var debug = require('debug')('serveur:server');
+var http = require('http');
+var fs = require('fs')
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '9999');
+app.set('port', port);
+
+/**nb g
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string' ?
+        'pipe ' + addr :
+        'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+var mysql = require('mysql')
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'dy',
+    password: '1234',
+    database: 'xwork'
+})
+var io = require("socket.io").listen(server)
+io.sockets.on("connection", function (socket, pseudo) {
+    console.log("un user c'est conneccte")
+    
+})
+module.exports = io
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/', function (req, res, next) {
+    res.render('accueil/index', { title: '<|> | Accueil', error: "", logo: "< | >", host: req.hostname });
+});
+
+module.exports = router;
